644217a488e2f2b1122a3ced450541c14b97991e
Daniel Halterman <danielhalterman@Daniels-MacBook-Pro.local>
2019-01-17 12:28:43 -0500

    Added in the model view and controller for registered applications site:

---
+++ app/controllers/registered_applications_controller.rb
@@ -0,0 +1,69 @@
+class RegisteredApplicationsController < ApplicationController
+
+  before_action :authenticate_user!, except: [:index, :show]
+
+  def create
+    @registered_app = current_user.registered_applications.new(app_params)
+
+    if @registered_app.save
+      flash[:notice] = "You successfully registered your app."
+      redirect_to @registered_app
+    else
+      flash.now[:alert] = "There was an error registering your app. Please try again."
+      render :new
+    end
+  end
+
+  def index
+    @registered_apps = []
+    @all_apps = RegisteredApplication.all
+    @all_apps.each do |app|
+      if app.user == current_user
+        @registered_apps << app
+      end
+    end
+  end
+
+  def show
+    @registered_app = RegisteredApplication.find(params[:id])
+  end
+
+  def new
+    @registered_app = RegisteredApplication.new
+  end
+
+  def edit
+    @registered_app = RegisteredApplication.find(params[:id])
+  end
+
+  def update
+    @registered_app = RegisteredApplication.find(params[:id])
+    @registered_app.assign_attributes(app_params)
+
+    if @registered_app.save
+      flash[:notice] = "Registered app was updated successfully."
+      redirect_to @registered_app
+    else
+      flash.now[:alert] = "There was an error updating the registered app. Please try again."
+      render :edit
+    end
+  end
+
+  def destroy
+    @registered_app = RegisteredApplication.find(params[:id])
+
+    if @registered_app.destroy
+      flash[:notice] = "\"#{@registered_app.name}\" was deleted successfully."
+      redirect_to registered_applications_path
+    else
+      flash.now[:alert] = "There was an error deleting the app. Try again?"
+      render :show
+    end
+  end
+
+   private
+
+   def app_params
+     params.require(:registered_applications).permit(:name, :url, :private)
+   end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  resources :registered_applications
+
   devise_for :users
   devise_for :models
+
+  get 'about' => 'welcome#about'
   root to: 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
