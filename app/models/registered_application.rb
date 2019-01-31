0422d834e2baf40e396bccae532dbc2fe1de0f7f
Daniel Halterman <danielhalterman@Daniels-MacBook-Pro.local>
2019-01-31 14:59:01 -0500

    Added events controller and trying to fix rspec

--- app/models/registered_application.rb
+++ app/models/registered_application.rb
@@ -1,9 +1,5 @@
 class RegisteredApplication < ApplicationRecord
   belongs_to :user
-<<<<<<< HEAD
-  has_many :events
-=======
 
   validates :user, presence: true
->>>>>>> BE_Blocmetrics_Register_Application
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   resources :registered_applications
 
+  namespace :api, defaults: { format: :json } do
+    match '/events', to: 'events#preflight', via: [:options]
+    resources :events, only: [:create]
+  end
+
   devise_for :users
   devise_for :models
 
