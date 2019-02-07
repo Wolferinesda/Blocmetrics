696486dbd2fe2613665cd61565c729a54007fe07
Daniel Halterman <danielhalterman@Daniels-MacBook-Pro.local>
2019-02-07 16:20:05 -0500

    Trying to fix events

--- app/controllers/api/events_controller.rb
+++ app/controllers/api/events_controller.rb
@@ -2,10 +2,10 @@ class API::EventsController < ApplicationController
   skip_before_action :verify_authenticity_token
   skip_before_action :authenticate_user!, only: [:create]
 
-before_filter :set_access_control_headers
+  before_action :set_access_control_headers
 
   def set_access_control_headers
-    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Origin'] = 'http://localhost'
     headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
     headers['Access-Control-Request-Method'] = '*'
     headers['Access-Control-Allow-Headers'] = 'Content-Type'
@@ -15,15 +15,15 @@ before_filter :set_access_control_headers
     registered_application = RegisteredApplication.find_by(url: request.env['HTTP_ORIGIN'])
     puts '==DEBUG: events_controller#create=='
     puts "registered application: #{registered_application}\n"
-    puts "url: #{request.env['HTTP_ORIGIN']}"
-    puts "request.env: #{request.env.inspect}"
+    puts "url: #{request.env['HTTP_ORIGIN']}\n"
+    puts "request.env: #{request.env.inspect}\n"
+    puts "event_params: #{event_params}"
 
     if registered_application.nil?
       render json: "Unregistered application", status: :unprocessable_entity
     end
 
-    @event = Event.new(event_params)
-    registered_application.event = registered_application
+    @event = registered_application.events.new(event_params)
     if @event.save
       render json: @event, status: :created
     else
